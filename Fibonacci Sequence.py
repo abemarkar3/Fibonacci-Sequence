0ba660a4ea3af26bc1e03726f11f09f2122f486b
Abemarkar23 <abemarkar23@gmail.com>
2017-09-04 18:09:47 -0700

    First Commit

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,11 @@
+''' Fibonacci Sequence - Enter a number and have the program generate the Fibonacci 
+	sequence to that number or to the Nth number.'''
+
+def Fibonacci(n):
+    if n < 2:
+        return n
+    return Fibonacci(n-1) + Fibonacci(n-2)
+
+
+
+print(Fibonacci(5))
